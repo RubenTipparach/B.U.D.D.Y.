e3951deb507e961d9b838b5d555f02db26da2678
JoeyCluett <joesunglassescool@gmail.com>
2017-03-22 20:22:28 -0500

    First code commit, Hello World

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+/*
+    Game thing in C++
+
+    FIRST COMPILE:
+        - Joey Cluett March 22, 2017 @ 8:21pm
+        - Ubuntu 14.04.1 Linux with
+            GCC 4.8.2
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+    cout << "Hello World" << endl;
+    return 0;
+}
